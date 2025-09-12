5f6cf4a2fba88bf50f8f60db4d0e358755c0bffd
wedosoft <alan@wedosoft.net>
2025-09-12 17:26:20 +0900

    restart based on linear.app

--- src/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export { Container } from "./container";
-export { Section } from "./section";
-export { Header } from "./header";
-export { MegaMenu } from "./mega-menu";
-export { Footer } from "./footer";
--- src/components/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { Button } from "./button";
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card";
-export { Logo, LogoIcon, LogoText } from "./logo";
