7af9ecf5ae0008d108cc791c606a97f1dacce8e1
wedosoft <alan@wedosoft.net>
2025-09-15 18:04:25 +0900

    default dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
             <body className="font-nanum-square">
                 <ThemeProvider
                     attribute="class"
-                    defaultTheme="system"
+                    defaultTheme="dark"
                     enableSystem
                     disableTransitionOnChange
                 >
